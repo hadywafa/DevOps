9d7ec8f8f4e8c421cf29dcd93f1daf370ff8c4cb
hady <hady.wafa@hadafsolutions.com>
2024-03-17 14:29:34 +0200

    save

--- Applications/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
+++ Applications/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09747729fee384ee65e7e8ced6ec7401b06306b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf7fad7234db3858650f2eae92f5f5237a801a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
